929658fd99f9e6427c61efbb83d28d72c6d2cce4
Heather Nuffer <zzzuzz@gmail.com>
2019-10-14 21:33:41 -0600

    Finished callbacks section

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -76,17 +76,69 @@ const lastTest2 = last(items, repeatThrice);
 console.log(lastTest2);
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);// sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+
+//Test 1 for sumNums()
+const sumNumsTest1 = sumNums(100, 200, (result) => `Sum: ${result}`);
+console.log(sumNumsTest1);
+
+//Test 2 for sumNums()
+const sumNumsTest2 = sumNums(200, -900, (result) => `Next sum: ${result}`);
+console.log(sumNumsTest2);
+
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+//Test 1 for multiplyNums()
+const repeatNum = function(num) {
+  let myStr = '';
+  for (let i=0; i<100; i++) {
+    myStr += num;
+  }
+  return myStr;
+}
+
+
+
+
+const multiplyTest1 = multiplyNums(3, 3, repeatNum);
+console.log(multiplyTest1);
+
+const multiplyTest2 = multiplyNums(5, 50, num => num * 1000);
+console.log(multiplyTest2);
+
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+//Test 1 for contains()
+
+function makeBoomBox(isFound) {
+  if (isFound) {
+    return `♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪`;
+  } else {
+    return `[{-_-}] ZZZzz zz z...`;
+  }
+}
+
+const containsTest1 = contains('cheese', items, makeBoomBox);
+console.log(containsTest1);
+
+//Test 2 for contains()
+const containsTest2 = contains('Pencil', items, makeBoomBox);
+console.log(containsTest2);
+
 
 /* STRETCH PROBLEM */
 
@@ -94,4 +146,39 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let mySet = new Set(array);
+  return cb([...mySet]);
 }
+
+//Test 1 for removeDuplicates()
+const itemsWithDuplicates = ['Pencil', 'Pencil','Pencil','Notebook', 'yo-yo', 'Gum', 'Pencil', 'Gum'];
+const testSet = new Set(itemsWithDuplicates);
+console.log([...testSet]);
+
+const formatArray = function(myArray) {
+  let returnArray = [];
+  for (let i=0; i<myArray.length; i++) {
+    returnArray.push(`Item #${i+1}: ${myArray[i]}`);
+  }
+  return returnArray;
+}
+
+const test1removeDuplicates = removeDuplicates(itemsWithDuplicates, formatArray);
+console.log(test1removeDuplicates);
+
+const shoutArray = function(myArray) {
+  let returnArray = [];
+  for (let i=0; i<myArray.length; i++) {
+    returnArray.push(`${myArray[i].toUpperCase()}`);
+  }
+  return returnArray;
+}
+
+const test2removeDuplicates = removeDuplicates(itemsWithDuplicates, shoutArray);
+console.log(test2removeDuplicates);
+
+
+
+
+
+
